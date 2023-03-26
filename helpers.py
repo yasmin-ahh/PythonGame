ec1c4fb6cb819b2b5f10325851c830b624bfd4f4
yasmin-ahh <yasminhassan240@gmail.com>
2023-03-26 17:19:43 +0400

    seperating functionality

---
+++ helpers.py
@@ -0,0 +1,84 @@
+from os.path import isfile, join
+from os import listdir
+import pygame
+
+pygame.init()
+
+pygame.display.set_caption("Platformer")
+
+WIDTH, HEIGHT = 1000, 800
+FPS = 60
+
+window = pygame.display.set_mode((WIDTH, HEIGHT))
+
+
+
+def flip(sprites):
+    return [pygame.transform.flip(sprite, True, False) for sprite in sprites]
+
+
+def load_sprite_sheets(dir1, dir2, width, height, direction=False):
+    path = join("assets", dir1, dir2)
+    images = [f for f in listdir(path) if isfile(join(path, f))]
+
+    all_sprites = {}
+
+    for image in images:
+        sprite_sheet = pygame.image.load(join(path, image)).convert_alpha()
+
+        sprites = []
+        for i in range(sprite_sheet.get_width() // width):
+            surface = pygame.Surface((width, height), pygame.SRCALPHA, 32)
+            rect = pygame.Rect(i * width, 0, width, height)
+            surface.blit(sprite_sheet, (0, 0), rect)
+            sprites.append(pygame.transform.scale2x(surface))
+
+        if direction:
+            all_sprites[image.replace(".png", "") + "_right"] = sprites
+            all_sprites[image.replace(".png", "") + "_left"] = flip(sprites)
+        else:
+            all_sprites[image.replace(".png", "")] = sprites
+
+    return all_sprites
+
+
+def get_block(size):
+    path = join("assets", "Terrain", "Terrain.png")
+    image = pygame.image.load(path).convert_alpha()
+    surface = pygame.Surface((size, size), pygame.SRCALPHA, 32)
+    rect = pygame.Rect(96, 0, size, size)
+    surface.blit(image, (0, 0), rect)
+    return pygame.transform.scale2x(surface)
+
+
+def get_background(WIDTH,HEIGHT, name):
+    image = pygame.image.load(join("assets", "Background", name))
+    _, _, width, height = image.get_rect()
+    tiles = []
+
+    for i in range(WIDTH // width + 1):
+        for j in range(HEIGHT // height + 1):
+            pos = (i * width, j * height)
+            tiles.append(pos)
+
+    return tiles, image
+
+
+def draw(window, background, bg_image, player, objects, offset_x, HEIGHT):
+    for tile in background:
+        window.blit(bg_image, tile)
+
+    for obj in objects:
+        obj.draw(window, offset_x)
+
+    player.draw(window, offset_x)
+
+    pygame.display.update()
+
+def generate_blocks(objects, block_count, block_size, direction="", height_off = 2, blocks_distance = 2, HEIGHT =0, Block=None):
+    if direction == "vertical":
+        for num in range(1, block_count+1):
+            objects.append(Block(block_size*blocks_distance, HEIGHT - block_size * num, block_size ))
+    elif direction == "horizontal":
+         for num in range(1, block_count):
+            objects.append(Block(block_size * num + block_size*blocks_distance, HEIGHT - block_size * height_off, block_size))
